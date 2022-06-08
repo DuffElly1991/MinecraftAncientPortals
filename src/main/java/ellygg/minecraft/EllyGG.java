5d3347688bebfdb47b97d9d61ba8e821d254f154
Elly <duffelly1991@gmail.com>
2022-06-08 13:46:06 +1200

    Version 0.3, final version for minecraft 1.18.*

--- src/main/java/ellygg/minecraft/EllyGG.java
+++ src/main/java/ellygg/minecraft/EllyGG.java
@@ -8,64 +8,66 @@ public class EllyGG implements ModInitializer {
 
     }
     /*
-        Crafting recipes
-            - cobbled deepslate = basalt + cobblestone
-                Like this recipe, probably going to keep
-                Like adding another use for basalt which doesn't do much given it has a unique method of renewal
-                Basalt is also a volcanic rock, and deepslate is found deeper in the world so might be formed through heat and pressure on stone
-                Like that recipe is implying that deepslate is significantly denser than cobblestone
-                Considered using 'deepslate = basalt + stone', but decided on cobble version because otherwise main use would be to immediately place and then mine
+		Build:
+			./gradlew build
+		Finish mod:
+			/home/elly/Documents/VS code/EllyGGMinecraft/bin/main/
+			copy all into zip, rename to jar
+	*/
 
-            - netherrack = gravel + blaze powder
-                Like this recipe and probably going to keep
-                Netherrack is weak so didn't want to make out of a durable block like stone
-                Wanted a source of heat to create it.  Chose blaze powder over lava bucket
-                Gravel and blaze powder are both renewable in the nether
+	/*
+		TO DO LIST
+			- cobbled deepslate
+				Do this like carpet mod, water + lava recipe but deep in the world
+					-- Maybe have a check for carpet and if it is present use carpet implementation instead of mine
+			- renewable sand source
+				From villager trade?
+				Purifying soul sand?
+					-- with lightning like what happens with copper
+					-- using dripstone like what happens with clay
+					-- using water like concrete
+				Recipe that is made from quartz?
 
-            - sand = soul sand + prismarine crystal
-                Unsure about recipe, might change
-                Like idea of converting soul sand into sand, via "purifying"
-                Like idea adding another use for guardian farms, but maybe could find a better fit
-                Considered splash potion of healing, but that would be weird as you'd be using something crafted from sand to make more sand
-                Might replace with lightning strike interaction like copper has
-                    Downside would be that lightning isn't consistently accessible
 
-            - soul soil = soul sand
-                Unsure about this recipe, might change
-                Wanted easy way to convert soul sand to soul soil
-                    Vanilla method is crafting soul campfire with soul sand, placing it, mining it - bad, tedious, probably unintended
+		IDEAS LIST
+			Un-dye blocks using cauldron to craft instead of standard crafting
 
-            - 8 clay = 8 dirt + water bucket
-                Temporary recipe, will be removed with wild update's mud
-                Could be significantly faster way of acquiring clay than the vanilla version so not ideal
-                Might replace with another temporary method, e.g. an attempt to implement dripstone drying mechanic
-
-            - 8 endstone = 8 granite + milk bucket
-                Temporary easter egg like recipe, will not stay
-                3 stage crafting needed, not ideal
-                Considering instead of crafting recipe using game interaction e.g. lava + water.
-                Maybe similar method to basalt creation
-                Although this would be a bit weird given that water and lava don't exist naturally in the end
-     */
-    /*
-        Possible recipes / methods to obtain blocks
-
-            cobweb - 9 string - ???
+			cobweb - 9 string - ???
             string - cobweb = 9 string - ???
                 Considering adding a method to convert between string and cobweb
                 Maybe don't want to add as cobweb isn't generally needed in large quantities
                     and it can be interesting to have items that you need to go out and find before you can use them
                 Another possibility would be to make cobweb a rare drop from cave spiders
 
-            gilded blackstone - blackstone + gold ingot
-                Possibly don't add method to make gilded blackstone, keep it as a rare/luxury block
-
             calcite - bone block + block of quartz = 2 calcite
-                Not sure if calcite should be renewable or luxury block
-                Not used as anything other than decoration
+                Not sure if calcite should be a renewable block or a luxury / status block
 
             tuff - ???
                 This block is kinda ugly and has no use yet
-                Will add crafting method if it becomes useful
+                Maybe add crafting method if it becomes useful
+
+
+		RULED OUT LIST
+			Don't add method to make gilded blackstone renewable, keep it as a rare/luxury block
+	*/
+	/*
+		CRAFTING RECIPES:
+            - soul soil <====> soul sand
+                Easy way to convert between soul sand to soul soil
+                    - Vanilla method is crafting soul campfire with soul sand, placing it and mining it to get soul soil
+					  I think this is bad because it is tedious and kinda pointless
+			
+			- Recipes to un-dye blocks
+				-- stained glass -> glass
+				-- stained glass pane -> glass pane
+
+            - netherrack = gravel + magma cream
+                Unsure about this recipe and probably going to keep for now
+                Netherrack is weak so didn't want to make out of a durable block like stone
+                Wanted a source of heat to create it.
+                Gravel and magma cream are both renewable in the nether
+				Something like stone generation would be ideal
+					Could consider making with flowing lava touching flowing lava.  Would need to not be able to replace lava source blocks
+
     */
 }

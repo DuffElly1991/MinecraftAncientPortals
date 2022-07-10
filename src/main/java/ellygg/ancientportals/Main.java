046dfabafbb1d8299c1d8affc19480123483d7b3
Elly <duffelly1991@gmail.com>
2022-07-10 22:51:08 +1200

    Version 0.1, creation of portal is working Currently is connecting up to the end dimension

--- src/main/java/ellygg/ancientportals/Main.java
+++ src/main/java/ellygg/ancientportals/Main.java
@@ -1,10 +1,19 @@
 package ellygg.ancientportals;
 
 import net.fabricmc.api.ModInitializer;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 
 public class Main implements ModInitializer {
     @Override
     public void onInitialize() {
-
+		CustomPortalBuilder.beginPortal()  
+        .frameBlock(Blocks.REINFORCED_DEEPSLATE)  
+        .lightWithItem(Items.LINGERING_POTION)  
+        .destDimID(new Identifier("the_end"))
+        .tintColor(16,64,16)  
+        .registerPortal();
     }
 }

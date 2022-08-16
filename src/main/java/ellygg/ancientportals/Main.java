12f87f92240006264f9925333d192516c16bb0d3
Elly <duffelly1991@gmail.com>
2022-08-17 11:11:06 +1200

    Hello world dimension

--- src/main/java/ellygg/ancientportals/Main.java
+++ src/main/java/ellygg/ancientportals/Main.java
@@ -8,12 +8,32 @@ import net.minecraft.util.Identifier;
 
 public class Main implements ModInitializer {
     @Override
-    public void onInitialize() {
+    public void onInitialize()
+	{
+		
+		//Create the dimension
+		Identifier dimensionIdentifier = createDimension();
+
+		//Create the portal to link up to the created dimension
+		createPortal(dimensionIdentifier);
+    }
+
+	public Identifier createDimension()
+	{
+		//to do replace this with link to the actual dimension
+		Identifier dimensionIdentifier = new Identifier("ancientportals", "longlostlands_dimension");
+		
+
+		return dimensionIdentifier;
+	}
+
+	public void createPortal(Identifier destination)
+	{
 		CustomPortalBuilder.beginPortal()  
         .frameBlock(Blocks.REINFORCED_DEEPSLATE)  
         .lightWithItem(Items.LINGERING_POTION)  
-        .destDimID(new Identifier("the_end"))
+        .destDimID(destination)
         .tintColor(16,64,16)  
         .registerPortal();
-    }
+	}
 }
